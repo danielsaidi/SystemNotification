9c8f39c91924d6ffc3a28184311dcbec94ebc2a1
Daniel Saidi <daniel.saidi@gmail.com>
2024-04-23 10:41:16 +0200

    Bump deployment targets

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "SystemNotification",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
-        .tvOS(.v14),
-        .watchOS(.v7),
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8),
         .visionOS(.v1)
     ],
     products: [
